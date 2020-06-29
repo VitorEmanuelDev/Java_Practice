97f4a70f95c14fcfce1790f9e96e34043b4c6bc1
VitorEmanuelDev <veslima3@gmail.com>
2020-06-28 21:16:37 -0300

    Write a Java program to print the sum of two numbers.

--- Basic_1/src/hello.java
+++ Basic_1/src/hello.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
-public class hello {
+//Write a Java program to print 'Hello' on screen and then print your name on a separate line.
 
-	//1. Write a Java program to print 'Hello' on screen and then print your name on a separate line.
-  public static void main(String[] args) {
+public class hello {
+  
+	public static void main(String[] args) {
 
 	  String name;
 	  System.out.println("Write your name:");
