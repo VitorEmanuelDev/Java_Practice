480d4d997fd8a621fb330b3f9c91b8a011c202b9
VitorEmanuelDev <veslima3@gmail.com>
2020-06-29 01:43:13 -0300

    Java program to check whether Java is installed

---
+++ Basic_1/src/javaInstalled.java
@@ -0,0 +1,16 @@
+//Write a Java program to check whether Java is installed on your computer.
+
+public class javaInstalled {
+
+	public static void main(String[] args) {
+		
+	    System.out.println("\nJava Version: " + System.getProperty("java.version"));
+	    System.out.println("Java Runtime Version: " + System.getProperty("java.runtime.version"));
+	    System.out.println("Java Home: " + System.getProperty("java.home"));
+	    System.out.println("Java Vendor: " + System.getProperty("java.vendor"));
+	    System.out.println("Java Vendor URL: " + System.getProperty("java.vendor.url"));
+	    System.out.println("Java Class Path: " + System.getProperty("java.class.path") + "\n");
+	    
+	 }
+	
+}
