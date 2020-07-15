56453b8b525ade44ed635c111ed2ee7788a56063
VitorEmanuelDev <veslima3@gmail.com>
2020-07-15 11:43:48 -0300

    Comments added. Java program to find the penultimate word of a sentence.

--- Basic_1/src/penultimateWord.java
+++ Basic_1/src/penultimateWord.java
@@ -14,7 +14,7 @@ import java.util.*;
      
 	 String str = in.nextLine(); //this string is one unit, and it's immutable unless we use a function
 	 
-	 String[] arrStr = str.split(" ");//the split function here is set to split the string into an array element whenever there's a space in it. this will make each word become a single element which will allow us to navigate the string array strArr with indexes
+	 String[] arrStr = str.split(" ");//the split function here is set to split the string into array elements whenever there's a space between them. this will make each word become a single element in an array structure which will allow us to navigate the string array strArr with indexes
 	 
 	 System.out.println("Penultimate word: " + arrStr[arrStr.length - 2]);// to show the penultimate word/element, we have to remember '\0' comes after the last word/element of any string and is counted in the attribute .length will count it. so putting strArr[strArr.length - 1] would get us the last word/element, but we want to get the penultimate word/element, so  let's write strArr[strArr.length - 2] instead.
 	 
