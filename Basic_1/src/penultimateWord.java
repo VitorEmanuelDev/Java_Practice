a8fa9b63e05dc0c246232a27656a537b16152a01
vitoremanueldev <veslima3@gmail.com>
2020-07-16 20:08:06 -0300

    test

--- Basic_1/src/penultimateWord.java
+++ Basic_1/src/penultimateWord.java
@@ -16,7 +16,7 @@ import java.util.*;
 	 
 	 String[] arrStr = str.split(" ");//the split function here is set to split the string into array elements whenever there's a space between them. this will make each word become a single element in an array structure which will allow us to navigate the string array strArr with indexes
 	 
-	 System.out.println("Penultimate word: " + arrStr[arrStr.length - 2]);// to show the penultimate word/element, we have to remember '\0' comes after the last word/element of any string, and that the attribute .length will count it. so, putting strArr[strArr.length - 1] would get us the last word/element, but we want to get the penultimate word/element, so let's write strArr[strArr.length - 2] instead.
+	 System.out.println("Penultimate word: "+ arrStr[arrStr.length - 2]);// to show the penultimate word/element, we have to remember '\0' comes after the last word/element of any string, and that the attribute .length will count it. so, putting strArr[strArr.length - 1] would get us the last word/element, but we want to get the penultimate word/element, so let's write strArr[strArr.length - 2] instead.
 	 
 	 }			
 	
