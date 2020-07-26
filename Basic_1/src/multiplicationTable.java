928b05fdea9257f65ee48ccbe821da06e21fb1cc
vitoremanueldev <veslima3@gmail.com>
2020-07-26 17:10:30 -0300

    Java program that takes a number as input and prints its multiplication table up to 10

--- Basic_1/src/multiplicationTable.java
+++ Basic_1/src/multiplicationTable.java
@@ -1,26 +1,24 @@
 import java.util.Scanner;
 
-//Write a Java program that takes a number as input and prints its multiplication table upto 10.
+//Write a Java program that takes a number as input and prints its multiplication table up to 10.
 
 public class multiplicationTable {
 
 	public static void main(String[] args){
 		  
-		  Scanner scan = new Scanner (System.in);
+		Scanner scan = new Scanner (System.in);
 	    
-		  System.out.print("Which table do you want to see?\n");
-		  int num = scan.nextInt();
-		  int result;
+		System.out.print("Which table do you want to see?\n");
+		int num = scan.nextInt();
+		int result;
 		  
-		  for(int i = 1; i <= 10; i++){
+		for(int i = 1; i <= 10; i++){
 			  
-			  result = num * i;
-			  System.out.println(num + " x " + i + " = " + result);
+			result = num * i;
+			System.out.println(num + " x " + i + " = " + result);
 			  
-		  }
-		  
-		  scan.close(); 
+		} 
 	    
-	  }
+	}
 	
 }
