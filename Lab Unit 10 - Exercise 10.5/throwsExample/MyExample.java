5cfbc7120ab4c484167ef151c3cdd198603fc040
vitoremanueldev <veslima3@gmail.com>
2021-08-03 01:12:26 -0300

    Lab Unit 10 - Exercise 10.5

---
+++ Lab Unit 10 - Exercise 10.5/throwsExample/MyExample.java
@@ -0,0 +1,11 @@
+package throwsExample;
+
+import java.io.IOException;
+
+public class MyExample {
+	
+	 void method()throws IOException{  
+	  throw new IOException("what the heck happened?!?!");  
+	 }  
+
+}
---
+++ Lab Unit 10 - Exercise 10.5/throwsExample/ThrowsExample.java
@@ -0,0 +1,14 @@
+package throwsExample;
+
+import java.io.IOException;
+
+public class ThrowsExample {
+	
+	 public static void main(String args[])throws IOException{
+	     
+		 MyExample test = new MyExample();  
+		 test.method();  
+	    
+	}  
+
+}
