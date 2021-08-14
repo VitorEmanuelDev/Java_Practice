d2784d0342914be79210e6a92020ce8cdac06dde
vitoremanueldev <veslima3@gmail.com>
2021-08-14 01:33:07 -0300

    test

--- Lab Unit 11 - Exercise 11.1/files/Files.java
+++ Lab Unit 11 - Exercise 11.1/files/Files.java
@@ -2,6 +2,10 @@ package files;
 
 import java.io.File;
 
+/*11.1 Write a program to create a text file in the path c:\java\abc.txt and check
+whether that file is exists. Using the command exists(), isDirectory(),
+isFile(), getName() and getAbsolutePath().*/
+
 public class Files {
 	
 	public static void main(String [] args){
